bb122305b2dbdad1e78d561b55f9e3f7320cb5da
Rushi Machale <98655276+rushikeshmachale@users.noreply.github.com>
2023-03-07 22:28:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 
 
+import pickle
+import numpy as np
 app = Flask(__name__)
 
 
@@ -10,8 +12,16 @@ def index():
 
 @app.route('/predict')
 def home():
-    return "Chalu ahe ha pn route"
+#     return "Chalu ahe ha pn route"
+    cgpa = request.form.get('cgpa')
+    iq = request.form.get('iq')
+    profile_score = request.form.get('profile_score')
 
+
+    input_query = np.array([[cgpa,iq,profile_score]])
+    result = model.predict(input_query)[0]
+    
+    return jsonify({'placement':result})
 
 if __name__ == '__main__':
     app.run(debug=True)
